dabd13c1648381a696d19f68f0798178a2fc23d0
Mi Gao <mimi.gao1991@hotmail.com>
2019-11-27 16:07:11 +0800

    simple yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show]
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+                          # restaurant_reviews_path(needs @restaurant)
+                          # new_restaurant_review_path(needs @restaurant)
+  end
+
+  resources :reviews, only: [:show]
+                          # review_path(needs @review)
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'The Dovetail',
+    address:      '9-10 Jerusalem Passage, Farringdon, London EC1V 4JP',
+    phone_number:        '02074907321',
+    category:     'belgian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:        '02075743919',
+    category:     'italian'
+  },
+  {
+    name:         'Min Jiang',
+    address:      '2-24 Kensington High St, Kensington, London W8 4PT',
+    phone_number:        '02073611988',
+    category:     'chinese'
+  },
+  {
+    name:         'Dinings',
+    address:      '22 Harcourt St, Marylebone, London W1H 4HH',
+    phone_number:        '02077230666',
+    category:     'japanese'
+  },
+  {
+    name:         'Clos Maggiore',
+    address:      '33 King St, Covent Garden, London WC2E 8JD',
+    phone_number:        '02073799696',
+    category:     'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
